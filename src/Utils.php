e0c36678a705d21f7bf4fef7da3593d4478b151d
Ashley <38263557+ularanigu@users.noreply.github.com>
2018-08-21 23:48:56 -0500

    Add utils class.

---
+++ src/Utils.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+/**
+ * Two-Factor plugin for Crystal Framework.
+ *
+ * @author  Nicholas English <nenglish6657@gmail.com>.
+ * @license <https://github.com/crystal-framework/two-factor-auth/blob/master/LICENSE> MIT License.
+ * @link    <https://github.com/crystal-framework/two-factor-auth> Source Code,
+ */
+
+namespace Crystal\Framework\Plugin\Auth\TwoFactor;
+
+/**
+ * This contains tools for the two-factor authentication method.
+ */
+class Utils
+{
+}
